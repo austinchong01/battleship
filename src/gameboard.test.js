fd14b1bcb7164cc9d49e3de094fbd501dcd1849b
Austin <austin.inhyuk.chong@gmail.com>
2025-04-20 14:41:02 -0700

    Fix Testing

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,22 +1,25 @@
-import Gameboard from './gameboard.js';
-import findSubArray from './compare.js';
+import Gameboard from "./gameboard.js";
+import { findSubArray } from "./operations.js";
 
-test('addShip 1', () => {
+test("addShip 1", () => {
   const gameboard = new Gameboard();
   const ship = gameboard.addShip([[0, 0]]);
   expect(ship.length).toBe(1);
   expect(ship.hits).toBe(0);
   expect(ship.sunk).toBe(false);
-  expect(gameboard.ships).toBe(1)
+  expect(gameboard.ships).toBe(1);
 });
 
-test('addShip 2', () => {
+test("addShip 2", () => {
   const gameboard = new Gameboard();
-  const ship = gameboard.addShip([[0, 0], [1, 0]]);
+  const ship = gameboard.addShip([
+    [0, 0],
+    [1, 0],
+  ]);
   expect(ship.length).toBe(2);
   expect(ship.hits).toBe(0);
   expect(ship.sunk).toBe(false);
-  expect(gameboard.ships).toBe(1)
+  expect(gameboard.ships).toBe(1);
 });
 
 test("receiveAttack 1", () => {
@@ -26,13 +29,16 @@ test("receiveAttack 1", () => {
   gameboard.receiveAttack([0, 2]);
   expect(ship.hits).toBe(1);
   gameboard.receiveAttack([1, 0]);
-  expect(findSubArray(gameboard.missed, [0, 2])).toBe(true)
-  expect(findSubArray(gameboard.missed, [0, 0])).toBe(false)
+  expect(findSubArray(gameboard.missed, [0, 2])).toBe(true);
+  expect(findSubArray(gameboard.missed, [0, 0])).toBe(false);
 });
 
 test("receiveAttack 2", () => {
   const gameboard = new Gameboard();
-  const ship = gameboard.addShip([[0, 0], [1, 0]]);
+  const ship = gameboard.addShip([
+    [0, 0],
+    [1, 0],
+  ]);
   gameboard.receiveAttack([0, 0]);
   gameboard.receiveAttack([0, 2]);
   expect(ship.hits).toBe(1);
@@ -46,9 +52,12 @@ test("receiveAttack 2", () => {
 
 test("allShipsSunk", () => {
   const gameboard = new Gameboard();
-  gameboard.addShip([[0, 0], [1, 0]]);
+  gameboard.addShip([
+    [0, 0],
+    [1, 0],
+  ]);
   expect(gameboard.allShipsSunk()).toBe(false);
   gameboard.receiveAttack([0, 0]);
   gameboard.receiveAttack([1, 0]);
   expect(gameboard.allShipsSunk()).toBe(true);
-})
+});
